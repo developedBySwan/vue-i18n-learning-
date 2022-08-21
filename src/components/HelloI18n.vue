ab1fc424f4e7fa7aa93ad756898e231baf553bb5
Swan-Yee <swanyeetun.2000@gmail.com>
2022-08-21 22:19:32 +0630

    add locale

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ $t("lan") }}</h1>
+    <p>{{ $t("message") }}</p>
+    <span>
+      {{ $t("friut.apple") }}
+    </span>
+    <span>
+      {{ $t("fruid.kiwi") }}
+    </span>
+    <div>
+      <button @click="change('en')">English</button>
+      <button @click="change('mm')">Myanmar</button>
+      <button @click="change('th')">Thailand</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n",
+  methods: {
+    change(lang) {
+      this.$i18n.locale = lang;
+    },
+  },
+};
+</script>
